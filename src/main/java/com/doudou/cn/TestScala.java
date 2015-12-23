1858c14b297332c9d11051b0f92dc479f87194a3
金亮 <jinliang@ihbaby.com>
2015-12-23 16:43:51 +0800

    Initial commit

---
+++ src/main/java/com/doudou/cn/TestScala.java
@@ -0,0 +1,14 @@
+package com.doudou.cn;
+
+/**
+ * Created by jinliang on 15/12/23.
+ *
+ *  java与scala 之间的交互
+ *
+ */
+public class TestScala {
+    public static void main(String[] args) {
+        ScalaFun fun = new ScalaFun();
+        fun.sayHello("你好");
+    }
+}
---
+++ src/main/scala/com/doudou/cn/ScalaFun.scala
@@ -0,0 +1,10 @@
+package com.doudou.cn
+
+/**
+  * Created by jinliang on 15/12/23.
+  */
+class ScalaFun {
+  def sayHello(content: String): Unit = {
+    println("----调用scala 函数----:" + content);
+  }
+}
